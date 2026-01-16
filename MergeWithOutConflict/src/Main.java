61ae9c925902ff47bf6626dfd533575c9db98eaa
GerardoOrgg <20253ds064@utez.edu.mx>
2026-01-15 21:03:45 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -1,6 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Main {
     static void main(String[] args) {
         System.out.println("Semana 2");
         System.out.println("Version Base");
+        System.out.println("2 + 2 = 4");
+        System.out.println("5 * 3 = 15");
     }
 }
