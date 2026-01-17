44a09108ee9c8ac1c963fba3010956d43d4daed9
GerardoOrgg <20253ds064@utez.edu.mx>
2026-01-16 23:06:05 -0600

    Actividad Practicas Buenas

---
+++ ActividadBuenasPracticas/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static int RESULTADO = 0;
+    public static int CONTADOR = 1;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int Entero = pedirEntero(sc, "Dame un numero del 1 al 10");
+        int Suma = SumarHastaN(Entero);
+        System.out.println(MensajeFinal(Suma));
+    }
+    public static int pedirEntero (Scanner sc, String mensaje){
+        System.out.println(mensaje);
+        return sc.nextInt();
+    }
+    public static int SumarHastaN (int Entero){
+        for(CONTADOR=1;CONTADOR<=Entero;CONTADOR++){
+            RESULTADO+=CONTADOR;
+        }
+        return RESULTADO;
+    }
+    public static String MensajeFinal (int numero){
+        return "El resultado es:"+RESULTADO;
+    }
+}
