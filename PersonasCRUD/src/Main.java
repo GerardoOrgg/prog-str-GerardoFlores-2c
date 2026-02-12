2747198c720a78a55c8d1df8d23d50e84e8f544c
GerardoOrgg <20253ds064@utez.edu.mx>
2026-02-11 23:28:20 -0600

    Personascrud terminada a medias medias

--- PersonasCRUD/src/Main.java
+++ PersonasCRUD/src/Main.java
@@ -10,43 +10,58 @@ public class Main {
         Persona persona = new Persona();
 
          do {
-             System.out.println("Menu");
+             System.out.println("             --- Menu  ---                ");
              System.out.println("1) Alta");
              System.out.println("2) Buscar por ID (solo activas)");
              System.out.println("3) Baja lógica por ID");
              System.out.println("4) Listar activas");
              System.out.println("5) Actualizar nombre por ID (solo activas)");
              System.out.println("0) Salir");
-             System.out.print("Ingrese la opcion");
+             System.out.print("Ingrese la opcion: ");
              opc = sc.nextInt();
 
              switch (opc){
                  case 1:
-                     int id = pedirdatos.Id("Ingresa el ID:", sc, personas, contador);
-                     String name = pedirdatos.name("Ingresa el nombre:", sc);
-                     boolean isactive = pedirdatos.isActivo("Esta activo?", sc);
+                     System.out.println("--- Alta ---");
+                     int id = pedirdatos.Id("Ingresa el ID: ", sc, personas, contador);
+                     String name = pedirdatos.name("Ingresa el nombre: ", sc);
+                     boolean isactive = pedirdatos.isActivo("Esta activo? ", sc);
                      persona = new Persona(id, name, isactive);
                      personas[contador] = persona;
                      contador++;
-                     System.out.println("Persona registrada correctamente.");
+                     System.out.println(" ---  Persona registrada correctamente.  ---- ");
                      break;
                  case 2:
-                     System.out.println("Buscar por ID");
-                     System.out.println("Ingrese el ID que quiere buscar");
+                     System.out.println("--- Buscar por ID ---");
+                     System.out.print("Ingrese el ID que quiere buscar: ");
                      int buscarid = sc.nextInt();
                      for (int i = 0; i < contador; i++) {
-                         if (personas[i].getId() == buscarid) {
-                             System.out.println("ID: " + personas[i].getId());
-                             System.out.println("Nombre: " + personas[i].getName());
-                             System.out.println("Activo: " + personas[i].isActive());
-                             System.out.println("-------------------");
+                         if (personas[i].isActive()) {
+                             if (personas[i].getId() == buscarid) {
+                                 System.out.println("ID: " + personas[i].getId());
+                                 System.out.println("Nombre: " + personas[i].getName());
+                                 System.out.println("Activo: " + personas[i].isActive());
+                                 System.out.println("-------------------");
+                             }
+                         } else {
+                             System.out.println(" -- La persona no esta activa -- ");
                          }
                      }
                      break;
                  case 3:
-                     System.out.println("Listas Activas");
+                     System.out.println("Ingrese el ID a dar de baja");
+                     buscarid = sc.nextInt();
+                     for (int i = 0; i < contador; i++) {
+                         if (personas[i].getId() == buscarid){
+                             personas[i].setActive(false);
+                         }
+                     }
+                     break;
+                 case 4:
+                     System.out.println("--- Listas Activas --- ");
                      for (int i = 0; i < contador; i++) {
                          if (personas[i].isActive()) {
+                             System.out.println("-------------------");
                              System.out.println("ID: " + personas[i].getId());
                              System.out.println("Nombre: " + personas[i].getName());
                              System.out.println("Activo: " + personas[i].isActive());
@@ -54,14 +69,18 @@ public class Main {
                          }
                      }
                      break;
-                 case 4:
-                     System.out.println("Actualizar nombre por ID");
-                     System.out.println("Ingresa el Id a actulizar");
+                 case 5:
+                     System.out.println("---- Actualizar nombre por ID ----");
+                     System.out.print("Ingresa el ID a actulizar: ");
                      buscarid = sc.nextInt();
                      for (int i = 0; i < contador; i++) {
-                         if (personas[i].getId() == buscarid){
-                             name = pedirdatos.name("Ingresa el nuevo nombre", sc);
-                             personas[i].setName(name);
+                         if (personas[i].isActive()) {
+                             if (personas[i].getId() == buscarid){
+                                 name = pedirdatos.name("Ingresa el nuevo nombre: ", sc);
+                                 personas[i].setName(name);
+                             }
+                         } else {
+                             System.out.println("La persona no esta activa");
                          }
                      }
                      break;
--- PersonasCRUD/src/PedirDatos.java
+++ PersonasCRUD/src/PedirDatos.java
@@ -7,12 +7,13 @@ public class PedirDatos {
     public int Id(String message, Scanner sc, Persona[] personas, int contador){
         int id;
         while(true){
-            System.out.println(message);
+            System.out.print(message);
             if(sc.hasNextInt()){
                 id = sc.nextInt();
                 sc.nextLine();
                 if(id <= 0){
-                    System.out.println("El id no puede ser negativo o cero");
+                    System.out.println(" -- El id no puede ser negativo o cero -- ");
+                    continue;
                 }
                 boolean repetido = false;
                 for(int i = 0; i < contador; i++){
@@ -22,12 +23,12 @@ public class PedirDatos {
                     }
                 }
                 if(repetido){
-                    System.out.println("El id ya está registrado, intenta otro.");
+                    System.out.println(" -- El id ya está registrado, intenta otro. -- ");
                 } else {
                     return id;
                 }
             } else {
-                System.out.println("El valor no es numérico");
+                System.out.println(" -- El valor no es numérico --");
                 sc.nextLine();
             }
         }
@@ -37,7 +38,7 @@ public class PedirDatos {
     public String name(String message, Scanner sc){
         String name;
         while(true){
-            System.out.println(message);
+            System.out.print(message);
             name = sc.nextLine();
 
             if(name.trim().isEmpty()){
@@ -51,7 +52,7 @@ public class PedirDatos {
     public boolean isActivo (String message, Scanner sc){
         boolean isactivo;
         while(true){
-            System.out.println(message + " (true/false)");
+            System.out.print(message + " (true/false) : ");
             if(sc.hasNextBoolean()){
                 isactivo = sc.nextBoolean();
                 sc.nextLine();
